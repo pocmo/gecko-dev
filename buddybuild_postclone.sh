065a34ea4eb9c5aec0751ddd3ed70064c547d044
Sebastian Kaspari <s.kaspari@gmail.com>
2017-05-10 15:48:54 -0700

    Install git cinnabar

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -2,3 +2,11 @@
 
 ./mach boostrap
 ./mach configure
+
+git clone https://github.com/glandium/git-cinnabar
+export PATH=$PATH:./git-cinnabar/
+git cinnabar download
+
+git remote set-url origin hg::https://hg.mozilla.org/mozilla-central
+git -c cinnabar.graft=true remote update
+
